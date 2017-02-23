d47b795ac982077b43d586c1a549184edd1905db
Knut Hühne <knut@k-nut.eu>
2017-02-23 18:28:40 +0100

    add CORS headers

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from controller import check_city, check_river, check_country
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/city")
 def city():
