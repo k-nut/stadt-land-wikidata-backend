1f24605ea49bf2f4035393c01c978e9a782fda91
Knut Hühne <knut@k-nut.eu>
2017-03-05 17:39:45 +0100

    return examples and deduplicate code

--- app.py
+++ app.py
@@ -1,35 +1,58 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-from controller import check_city, check_river, check_country
+from controller import check_city, check_river, check_country, example_river, example_city, example_country
 
 
 app = Flask(__name__)
 CORS(app)
 
+
+def check(function, value):
+    result = function(value)
+    return jsonify({'data': result})
+
 @app.route("/city")
 def city():
-    result = check_city(request.args['name'])
-    if result:
-        return jsonify({'data': {'link': result, 'correct': True}})
-    else:
-        return jsonify({'data': {'correct': False}})
+    return check(check_city, request.args['name'])
+
 
 @app.route("/river")
 def river():
-    result = check_river(request.args['name'])
-    if result:
-        return jsonify({'data': {'link': result, 'correct': True}})
-    else:
-        return jsonify({'data': {'correct': False}})
+    return check(check_river, request.args['name'])
+
 
 @app.route("/country")
 def country():
-    result = check_country(request.args['name'])
+    return check(check_country, request.args['name'])
+
+
+@app.route("/river_examples")
+def river_examples():
+    result = example_river(request.args['letter'])
     if result:
-        return jsonify({'data': {'link': result, 'correct': True}})
+        return jsonify({'data': result})
     else:
-        return jsonify({'data': {'correct': False}})
+        return jsonify({'data': None})
+
+
+@app.route("/city_examples")
+def city_examples():
+    result = example_city(request.args['letter'])
+    if result:
+        return jsonify({'data': result})
+    else:
+        return jsonify({'data': None})
+
+
+@app.route("/country_examples")
+def country_examples():
+    result = example_country(request.args['letter'])
+    if result:
+        return jsonify({'data': result})
+    else:
+        return jsonify({'data': None})
+
 
 if __name__ == "__main__":
     app.run()
--- controller.py
+++ controller.py
@@ -1,6 +1,21 @@
 import requests
 
 
+def make_query(name, template, entity):
+    url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+    query = template.format(name=name)
+    data = requests.get(url, params={'query': query, 'format': 'json'}).json()
+    results = data['results']['bindings']
+    if results:
+        return {'link': results[0]['item']['value'],
+                'correct': True,
+                'name': name,
+                'entity': entity}
+    return {'correct': False,
+            'name': name,
+            'entity': entity}
+
+
 def check_city(name):
     template = """SELECT ?item
     WHERE
@@ -11,14 +26,7 @@ def check_city(name):
     }}
 
     """
-
-    url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
-    query = template.format(name=name)
-    data = requests.get(url, params={'query': query, 'format': 'json'}).json()
-    results = data['results']['bindings']
-    if results:
-        return results[0]['item']['value']
-    return False
+    return make_query(name, template, "city")
 
 
 def check_country(name):
@@ -29,14 +37,7 @@ def check_country(name):
       ?item ?label "{name}"@de .
     }}
     """
-
-    url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
-    query = template.format(name=name)
-    data = requests.get(url, params={'query': query, 'format': 'json'}).json()
-    results = data['results']['bindings']
-    if results:
-        return results[0]['item']['value']
-    return False
+    return make_query(name, template, "country")
 
 
 def check_river(name):
@@ -49,11 +50,61 @@ def check_river(name):
     }}
 
     """
+    return make_query(name, template, "river")
 
+
+def example_river(letter):
+    template = """SELECT ?item ?itemLabel ?length
+    WHERE
+    {{
+      ?item wdt:P31/wdt:P279* wd:Q4022 .
+      ?item wdt:P17 wd:Q183 .
+      ?item rdfs:label ?itemLabel .
+      ?item wdt:P2043 ?length .
+      filter(lang(?itemLabel) = "de") .
+      filter(strStarts(lcase(?itemLabel), "{letter}"))
+    }}
+    order by desc(?length)
+    limit 5"""
     url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
-    query = template.format(name=name)
+    query = template.format(letter=letter.lower())
     data = requests.get(url, params={'query': query, 'format': 'json'}).json()
-    results = data['results']['bindings']
-    if results:
-        return results[0]['item']['value']
-    return False
+    results = {"examples": data['results']['bindings'], "entity": "river"}
+    return results or None
+
+
+def example_country(letter):
+    template = """SELECT ?item ?itemLabel ?length
+    WHERE
+    {{
+      ?item wdt:P31/wdt:P279* wd:Q6256 .
+      ?item rdfs:label ?itemLabel .
+      filter(lang(?itemLabel) = "de") .
+      filter(strStarts(lcase(?itemLabel), "c"))
+    }}
+    order by desc(?length)
+    limit 5"""
+    url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+    query = template.format(letter=letter.lower())
+    data = requests.get(url, params={'query': query, 'format': 'json'}).json()
+    results = {"examples": data['results']['bindings'], "entity": "country"}
+    return results or None
+
+
+def example_city(letter):
+    template = """SELECT ?item ?itemLabel ?length
+    WHERE
+    {{
+      ?item wdt:P31/wdt:P279* wd:Q515 .
+      ?item wdt:P17 wd:Q183 .
+      ?item rdfs:label ?itemLabel .
+      filter(lang(?itemLabel) = "de") .
+      filter(strStarts(lcase(?itemLabel), "c"))
+    }}
+    order by desc(?length)
+    limit 5"""
+    url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
+    query = template.format(letter=letter.lower())
+    data = requests.get(url, params={'query': query, 'format': 'json'}).json()
+    results = {"examples": data['results']['bindings'], "entity": "city"}
+    return results or None
